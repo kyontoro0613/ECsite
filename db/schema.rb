cef87c26631f1e76cbb19c1eddbf4bd7cd65e454
kyontoro0613 <kyontoro7@gmai.com>
2018-12-16 19:55:49 +0900

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_16_085550) do
+ActiveRecord::Schema.define(version: 2018_12_16_101946) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "ecs", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +43,14 @@ ActiveRecord::Schema.define(version: 2018_12_16_085550) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_ecs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_ecs_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+item1 = Item.create!(
+  name: 'REGZA 50M520X [50インチ]',
+  price: 88000,
+  description: "滑らかなグラデーションや細かいディテールを再現する新映像エンジン「レグザエンジン Evolution」を搭載した、BS/CS 4Kチューナー内蔵の50V型液晶テレビ。\n絵柄に応じて適切な復元処理を実施し、ノイズを抑えながら4K放送本来の美しさを再現する「BS/CS 4KビューティX」技術を搭載。\n新開発のバスレフ型フルレンジスピーカーと、レグザサウンドイコライザー・ハイブリッドにより、低音から高音までバランスのよいサウンドを再現する。",
+)
+
+item2 = Item.create!(
+  name: 'VIERA TH-55EX850 [55インチ]',
+  price: 157000,
+  description: "独自設計の高輝度パネルを採用し、バックライトの光量を高め、高コントラスト描写が可能な、55型デジタルハイビジョンテレビ。\nハイレゾ対応ツイーター、ミッドレンジスピーカー、ウーハー、クアッド・パッシブラジエーターなど、サウンド面にも注力している。\nテレビもネット動画も切り替えずに、1画面で選んで見られる「アレコレチャンネル」機能を搭載。"
+)
+
+item3 = Item.create!(
+  name: 'BRAVIA KJ-49X9000F [49インチ]',
+  price: 12000,
+  description: "4K高画質プロセッサー「X1 Extreme」や高輝度技術「X-tended Dynamic Range PRO」を搭載した、49V型の4K液晶テレビ。\n独自技術の「X-Motion Clarity」により、動きの速い映像を「なめらか」かつ「くっきり」と再現。スポーツやアクションシーンでリアルな映像を楽しめる。\n「Android TV」機能を搭載し、リモコンに話しかけるだけでテレビ放送や録画番組、ネット動画のコンテンツを簡単に検索できる。"
+)
+
+item1.image.attach(io: File.open(Rails.root.join('db/images/1.jpg')), filename: "face.jpg", content_type: "image/jpg")
+
+item2.image.attach(io: File.open(Rails.root.join('db/images/2.jpg')), filename: "face.jpg", content_type: "image/jpg")
+
+item3.image.attach(io: File.open(Rails.root.join('db/images/K0001051426.jpg')), filename: "face.jpg", content_type: "image/jpg")
