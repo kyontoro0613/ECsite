bb4577e80696e7796afa32eeeb822a507a5b549e
kyontoro0613 <kyontoro7@gmai.com>
2018-12-23 19:59:45 +0900

    serch

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,10 +1,14 @@
 class ItemController < ApplicationController
   def index
-    @items = Item.all
+key_word = params[:name]
+    if key_word.blank?
+       @items = Item.all
+    else
+       @items = Item.where("(name like ?) OR (description like ?)", "%#{key_word}%", "%#{key_word}%")
+    end
   end
-
   def show
-     @item = Item.find_by(id: params[:id])
+     @items = Item.find_by(id: params[:id])
 
   end
 end
